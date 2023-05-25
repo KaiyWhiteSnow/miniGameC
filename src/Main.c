a02e9c1e4caa9ef8bf7ebc52e55d933fafe5ec35
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-05-25 10:38:53 +0200

    update: Responsivity, Reset

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.h>
 #include <SFML/System/Vector2.h>
 #include <SFML/Window/VideoMode.h>
+#include <stdio.h>
 
 /* Prepare textures */
 sfSprite* initializeSprite(const char* texture);
@@ -12,6 +13,8 @@ sfBool isPlayer1MovingLeft = sfFalse;
 sfBool isPlayer1MovingRight = sfFalse;
 sfBool isPlayer2MovingLeft = sfFalse;
 sfBool isPlayer2MovingRight = sfFalse;
+sfBool keyPressFlag = sfFalse;
+sfBool isBallMoving = sfTrue;
 
 /* Set movement speed for both players */
 float player1MovementSpeed = 300.0f;
@@ -20,13 +23,15 @@ float player2MovementSpeed = 300.0f;
 /* Set ball velocity */
 sfVector2f ballVelocity = {400.0f, 400.0f};
 
+/* Reset velocity (ball stays in place) */
+sfVector2f resetVelocity = {0.0f, 0.0f};
+
 /* Set initial positions for both players and ball */
-sfVector2f playerOnePosition = {350.0f, 540.0f};
-sfVector2f playerTwoPosition = {350.0f, 10.0f};
-sfVector2f ballPosition = {350.0f, 250.0f};
+sfVector2f playerOnePosition = {0.0f, 540.0f};
+sfVector2f playerTwoPosition = {0.0f, 10.0f};
+sfVector2f ballPosition = {0.0f, 0.0f};
 
-void updateCollisions(sfSprite* playerOne, sfSprite* playerTwo, sfSprite* ball, sfVector2f* ballVelocity, sfVector2f* ballPosition, sfVideoMode mode);
-
+void updateCollisions(sfSprite* playerOne, sfSprite* playerTwo, sfSprite* ball, sfVector2f* ballVelocity, sfVector2f* ballPosition, sfVideoMode mode, sfVector2f* resetVelocity, sfBool keyPressFlag);
 
 int main()
 {
@@ -48,6 +53,16 @@ int main()
     sfSprite_setPosition(ball, ballPosition);
 
     sfClock* clock = sfClock_create();
+    /* Get bounds for characters */
+    sfFloatRect player1Bounds = sfSprite_getGlobalBounds(playerOne);
+    sfFloatRect player2Bounds = sfSprite_getGlobalBounds(playerTwo);
+    sfFloatRect ballBounds = sfSprite_getGlobalBounds(ball);
+
+    /* Set player into place */
+    float xCoordinate = mode.width / 2 - player1Bounds.width / 2;
+    playerOnePosition.x = xCoordinate;
+    playerTwoPosition.x = xCoordinate;
+    ballPosition.x = xCoordinate;
 
     /* Start the game loop */
     while (sfRenderWindow_isOpen(window))
@@ -62,6 +77,12 @@ int main()
                 sfRenderWindow_close(window);
             else if (event.type == sfEvtKeyPressed)
             {
+                if (isBallMoving == sfFalse)
+                {
+                    keyPressFlag = sfTrue;
+                    resetBallVelocity(&ballVelocity, keyPressFlag);
+                }
+            
                 // Movement
                 sfKeyCode keyCode = event.key.code;
                 if (keyCode == sfKeyRight)
@@ -115,7 +136,7 @@ int main()
             player1Movement.x -= player1MovementSpeed * dt;
         }
         sfVector2f newPlayer1Position = {playerOnePosition.x + player1Movement.x, playerOnePosition.y};
-        sfFloatRect player1Bounds = sfSprite_getGlobalBounds(playerOne);
+        
         if (newPlayer1Position.x + player1Bounds.width <= mode.width && newPlayer1Position.x >= 0.0f)
         {
             playerOnePosition = newPlayer1Position;
@@ -133,7 +154,6 @@ int main()
             player2Movement.x -= player2MovementSpeed * dt;
         }
         sfVector2f newPlayer2Position = {playerTwoPosition.x + player2Movement.x, playerTwoPosition.y};
-        sfFloatRect player2Bounds = sfSprite_getGlobalBounds(playerTwo);
         if (newPlayer2Position.x + player2Bounds.width <= mode.width && newPlayer2Position.x >= 0.0f)
         {
             playerTwoPosition = newPlayer2Position;
@@ -143,14 +163,14 @@ int main()
         /* Update ball position */
         sfVector2f ballMovement = {ballVelocity.x * dt, ballVelocity.y * dt};
         sfVector2f newBallPosition = {ballPosition.x + ballMovement.x, ballPosition.y + ballMovement.y};
-        sfFloatRect ballBounds = sfSprite_getGlobalBounds(ball);
-
+        
         /* Update ball position */
         ballPosition = newBallPosition;
         sfSprite_setPosition(ball, ballPosition);
         
         /* Update collisions */
-        updateCollisions(playerOne, playerTwo, ball, &ballVelocity, &ballPosition, mode);
+        updateCollisions(playerOne, playerTwo, ball, &ballVelocity, &ballPosition, mode, &resetVelocity, keyPressFlag);
+
 
         /* Clear the screen */
         sfRenderWindow_clear(window, sfBlack);
@@ -187,10 +207,9 @@ sfSprite* initializeSprite(const char* texture)
     return sprite;
 }
 
-void updateCollisions(sfSprite* playerOne, sfSprite* playerTwo, sfSprite* ball, sfVector2f* ballVelocity, sfVector2f* ballPosition, sfVideoMode mode)
+void updateCollisions(sfSprite* playerOne, sfSprite* playerTwo, sfSprite* ball, sfVector2f* ballVelocity, sfVector2f* ballPosition, sfVideoMode mode, sfVector2f* resetVelocity, sfBool keyPressFlag)
 {
     sfFloatRect ballBounds = sfSprite_getGlobalBounds(ball);
-
     /* Check ball collision with player 1 */
     sfFloatRect player1Bounds = sfSprite_getGlobalBounds(playerOne);
     if (sfFloatRect_intersects(&player1Bounds, &ballBounds, NULL))
@@ -226,7 +245,25 @@ void updateCollisions(sfSprite* playerOne, sfSprite* playerTwo, sfSprite* ball, 
         ballPosition->x = mode.width / 2 - ballBounds.width / 2;
         ballPosition->y = mode.height / 2 - ballBounds.height / 2;
         
-        // Reverse ball's vertical velocity
-        ballVelocity->y = -ballVelocity->y;
+        /* Redefined - May cause issues later on, find a fix */
+        float xCoordinate = mode.width / 2 - player1Bounds.width / 2;
+        playerOnePosition.x = xCoordinate;
+        playerTwoPosition.x = xCoordinate;
+
+        // Reset ball velocity
+        ballVelocity->x = resetVelocity->x;
+        ballVelocity->y = resetVelocity->y;
+        
+        isBallMoving = sfFalse;
     }
+}
+
+void resetBallVelocity(sfVector2f* ballVelocity, sfBool keyPressFlag)
+{
+    keyPressFlag = sfFalse;
+    isBallMoving = sfTrue;
+
+    // Set a new non-zero velocity to make the ball move
+    ballVelocity->x = 400.0f;
+    ballVelocity->y = 400.0f;
 }
